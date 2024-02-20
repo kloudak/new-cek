4c0e2466d9b00e5e0351303cf5f9afa26eec62f5
Karel Klouda <karel.klouda@fit.cvut.cz>
2024-02-20 20:00:09 +0100

    adding a Person model

--- web/models.py
+++ web/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Person(models.Model):
+    SEX_CHOICES = (
+        ('M', 'Male'),
+        ('F', 'Female'),
+    )
+    
+    firstname = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField(null=True, blank=True)
+    place_of_birth = models.CharField(max_length=100, null=True, blank=True)
+    place_of_death = models.CharField(max_length=100, null=True, blank=True)
+    place_of_birth_original_name = models.CharField(max_length=100, null=True, blank=True)
+    place_of_death_original_name = models.CharField(max_length=100, null=True, blank=True)
+    remark = models.TextField(null=True, blank=True)
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES)
+    pseudonym_for = models.ForeignKey('self', on_delete=models.SET_NULL, null=True, blank=True, related_name='pseudonyms')
+
+    def __str__(self):
+        return f"{self.firstname} {self.surname}"
