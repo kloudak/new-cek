67537f51209435f315fc244f138d32bc0101dbdf
Karel Klouda <karel.klouda@fit.cvut.cz>
2024-02-24 22:21:59 +0100

    Persons in Admin are displayed as a table with many solumns.

--- web/admin.py
+++ web/admin.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 
 from .models import Person
 
-# Register your models here.
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ('surname', 'firstname', 'date_of_birth', 'date_of_death', 'sex', 'pseudonym_for')
+    search_fields = ('surname', 'firstname', 'date_of_birth', 'date_of_death')
+    ordering = ('surname', 'firstname', 'date_of_birth', 'date_of_death', 'sex', 'pseudonym_for')
 
-admin.site.register(Person)
+admin.site.register(Person, PersonAdmin)
