171b0168081adbb4236658a0677a2e60fa77f4f4
Karel Klouda <karel.klouda@fit.cvut.cz>
2024-02-28 16:35:32 +0100

    Displaying id in admin both for Book and Person, also displaying year and author_xml for Book

--- web/admin.py
+++ web/admin.py
@@ -3,9 +3,10 @@ from django.contrib import admin
 from .models import Person, Authorship, Book
 
 class PersonAdmin(admin.ModelAdmin):
-    list_display = ('surname', 'firstname', 'date_of_birth', 'date_of_death', 'sex', 'pseudonym_for')
-    search_fields = ('surname', 'firstname', 'date_of_birth', 'date_of_death')
-    ordering = ('surname', 'firstname', 'date_of_birth', 'date_of_death', 'sex', 'pseudonym_for')
+    list_display = ('id','surname', 'firstname', 'date_of_birth', 'date_of_death', 'sex', 'pseudonym_for')
+    list_display_links = ('surname',)
+    search_fields = ('id','surname', 'firstname', 'date_of_birth', 'date_of_death')
+    ordering = ('id','surname', 'firstname', 'date_of_birth', 'date_of_death', 'sex', 'pseudonym_for')
 
 class AuthorshipInline(admin.TabularInline):
     model = Authorship
@@ -13,7 +14,10 @@ class AuthorshipInline(admin.TabularInline):
 
 class BookAdmin(admin.ModelAdmin):
     inlines = (AuthorshipInline,)
-    list_display = ('title',)
+    list_display = ('id','title','year','author_xml')
+    list_display_links = ('title',)
+    search_fields = ('id','title','year','author_xml')
+    ordering = ('id','title','year','author_xml')
 
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Book, BookAdmin)
