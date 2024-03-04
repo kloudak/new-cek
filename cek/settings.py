212377fd77ab4a3b0006c964a6ba79b3e3aee7fb
Karel Klouda <karel.klouda@fit.cvut.cz>
2024-03-04 11:18:04 +0100

    Moving STATIC_URL from settings.py to config.py

--- cek/settings.py
+++ cek/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 
-from .config import SECRET_KEY, DATABASE_PASSWORD, DEBUG, ALLOWED_HOSTS, DATABASES
+from .config import SECRET_KEY, DATABASE_PASSWORD, DEBUG, ALLOWED_HOSTS, DATABASES, STATIC_URL
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -107,7 +107,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+# STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
