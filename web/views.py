3717a70cd8bb0c5c597f66877be4b209fd4c83fe
Karel Klouda <karel.klouda@fit.cvut.cz>
2024-03-04 08:54:14 +0100

    Author detail view added.

--- web/views.py
+++ web/views.py
@@ -1,21 +1,52 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-from django.db.models import Count
+from django.shortcuts import render, get_object_or_404
+from django.db import models
 from .models import Person, Book, Authorship
+from .utils import years_difference
 
 def index(request):
     return render(request, "web/index.html")
 
 # AUTHORS
 def author_list(request):
-    persons = Person.objects.annotate(num_books=Count('authorships__book')).order_by('surname', 'firstname')
+    persons = Person.objects.annotate(num_books=models.Count('authorships__book')).order_by('surname', 'firstname')
     return render(request, "web/author_list.html", {
         'persons': persons
     })
 
 def author_detail(request, id):
-    return render(request, "web/author_detail.html", {
-        "id" : id
+    # Fetch the person and their related books directly
+    author = get_object_or_404(Person, id=id)
+
+    # Age at deathday
+    if author.date_of_birth and author.date_of_death:
+        age = years_difference(author.date_of_birth, author.date_of_death)
+    else:
+        age = None
+    
+    # Pseudonyms
+    pseudonyms = author.pseudonyms.all()
+    
+    # Fetch Authorship instances where the person is a direct author or a pseudonym
+    authorships = Authorship.objects.filter(
+        models.Q(person_id=id) | 
+        models.Q(person__pseudonym_for_id=id)
+    ).select_related('book', 'person')
+
+    # Extract unique books from the authorships
+    books_dict = {}
+    for authorship in authorships:
+        books_dict[authorship.book.id] = authorship.book
+
+    # Extract books from the dictionary and sort them by year
+    books = sorted(books_dict.values(), key=lambda book: (book.year if book.year else 0))
+
+    print(len(books), books)
+    return render(request, 'web/author_detail.html', {
+        'author': author, 
+        'books': books,
+        'pseudonyms': pseudonyms,
+        'age': age
     })
 
 # BOOKS
