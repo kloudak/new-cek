66cdf37a2d1feeb0df71f10b0d12e55bb5d23efb
Karel Klouda <karel.klouda@fit.cvut.cz>
2024-03-15 11:44:57 +0100

    Poem model is now registered and displayed in Admin.

--- web/admin.py
+++ web/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Person, Authorship, Book
+from .models import Person, Authorship, Book, Poem
 
 class PersonAdmin(admin.ModelAdmin):
     list_display = ('id','surname', 'firstname', 'date_of_birth', 'date_of_death', 'sex', 'pseudonym_for')
@@ -10,14 +10,26 @@ class PersonAdmin(admin.ModelAdmin):
 
 class AuthorshipInline(admin.TabularInline):
     model = Authorship
-    extra = 1  # How many rows to show by default
+    extra = 1
+
+class PoemInline(admin.StackedInline):
+    model = Poem
+    fields = ['order_in_book'] 
+    extra = 0
 
 class BookAdmin(admin.ModelAdmin):
-    inlines = (AuthorshipInline,)
+    inlines = (AuthorshipInline, PoemInline)
     list_display = ('id','title','year','author_xml')
-    list_display_links = ('title',)
+    list_display_links = ('id', 'title')
     search_fields = ('id','title','year','author_xml')
     ordering = ('id','title','year','author_xml')
 
+class PoemAdmin(admin.ModelAdmin):
+    list_display = ('id','title', 'book')
+    list_display_links = ('title',)
+    search_fields = ('id','title', 'book')
+    ordering = ('id','title')
+
 admin.site.register(Person, PersonAdmin)
 admin.site.register(Book, BookAdmin)
+admin.site.register(Poem, PoemAdmin)
