aba707ed2dc9befbd95b77e9ee1837628602f6c5
Karel Klouda <karel.klouda@fit.cvut.cz>
2024-10-28 10:41:59 +0100

    Displaying files and side note in author detail.

--- web/urls.py
+++ web/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from . import views
 
@@ -32,3 +34,7 @@ urlpatterns = [
     path("edicni-poznamka", views.editorial_note, name="editorial_note"),
     path('robots.txt', views.robots, name='robots'),
 ]
+
+# Serve media files during development
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
